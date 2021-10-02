58188f334f7b94c1e4e9e61ce993b94625605cc9
theDaveB <davebloisesquire@gmail.com>
2021-10-02 12:39:42 -0700

    froze table names in db

--- models/VotesMain.js
+++ models/VotesMain.js
@@ -3,50 +3,49 @@ const sequelize = require('../config/connections');
 
 class VotesMain extends Model {}
 
-VotesMain.init(
-  {
+VotesMain.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     question: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     option1: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     option2: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     option3: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     option4: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'User',
-        key: 'id'
-      }
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'User',
+            key: 'id'
+        }
     },
     time_to_vote: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-  },
-  {
-    sequelize
-  }
-);
+}, {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'VotesMain',
+});
 
 module.exports = VotesMain;
