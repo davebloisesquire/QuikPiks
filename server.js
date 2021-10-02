76c0e6ec0d612e87cf98b9f3fb0bd5cf98be1498
theDaveB <davebloisesquire@gmail.com>
2021-10-02 11:54:28 -0700

    models updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connections');
 
 // Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
