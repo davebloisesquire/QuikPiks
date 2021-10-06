797393e18941cd63495fe8cb30510a4ff56e5da4
CarlosC074 <carlos.castano74@gmail.com>
2021-10-05 18:42:11 -0700

    Added placeholder template files, along with routes that implement authorization middleware to prevent non-logged in users from accessing the page

--- models/VotesMain.js
+++ models/VotesMain.js
@@ -1,49 +1,51 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 
 class VotesMain extends Model {}
 
-VotesMain.init(
-  {
+VotesMain.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     question: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     option1: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     option2: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     option3: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     option4: {
-      type: DataTypes.STRING,
-      allowNull: true,
+        type: DataTypes.STRING,
+        allowNull: true,
     },
     user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'User',
-        key: 'id'
-      }
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'User',
+            key: 'id'
+        }
     },
     time_to_vote: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-  },
-);
+}, {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'VotesMain',
+});
 
 module.exports = VotesMain;
