65a353adc22544b8af99bd68c9a4b64dbbbd9f1f
CarlosC074 <carlos.castano74@gmail.com>
2021-10-08 21:01:32 -0700

    Session persistence works to keep users logged in. Fixed style issues created in template creation (added a missing class to the body tag).

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,7 +27,7 @@ router.get('/home', withAuth, async (req, res) => {
     try {
       const userData = await User.findAll({
         attributes: { exclude: ['password'] },
-        order: [['name', 'ASC']],
+        order: [['username', 'ASC']],
       }
       );
       const users = userData.map((project) => project.get({ plain: true }));
